ef54cea510cf1d595666bc8e7001003955b49de0
unknown <sebastianwilches2@gmail.com>
2022-05-11 00:11:46 -0500

    Se agrego la consulta del clima e historial de busqueda

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { inquirerMenu, pausaMenu, inputInquirer, listarLugares } = require("./helpers/inquirerMenu");
 const { Busqueda } = require("./models/busqueda");
 
-console.log(process.env);
+// console.log(process.env); Para mirar variables de entorno declaradas
 
 const main = async () => {
 
@@ -17,25 +17,41 @@ const main = async () => {
             case 1: //Buscar ciudad
                 //Preguntar la ciudad
                 const consultaLugar = await inputInquirer('Ingrese una ciudad');
+
                 //Hacer la consulta HTTP
                 const lugares = await busquedas.ciudad(consultaLugar);
+
                 //Seleccionar una ciudad
                 const idLugar = await listarLugares(lugares);
+                if(idLugar ==='0') continue; //Se sale de esta iteracion                
                 const nombreLugarSeleccionado = lugares.find( l => l.id===idLugar);
-                console.log(nombreLugarSeleccionado);
-                // console.log({idLugar});
+                
+                //Guardar en persistencia
+                busquedas.agregarHistorial(nombreLugarSeleccionado.nombre);
+
                 //Hacer consulta del clima en OpenWeather
+                const clima = await busquedas.consultarClimaCoords(nombreLugarSeleccionado.latitud, nombreLugarSeleccionado.longitud);
+                // console.log(clima);
+
                 //Mostrar consulta
-                console.log('Info de la ciudad');
-                console.log('ID', nombreLugarSeleccionado.id);
-                console.log('Nombre', nombreLugarSeleccionado.nombre);
-                console.log('Latitud', nombreLugarSeleccionado.latitud);
-                console.log('Longitud', nombreLugarSeleccionado.longitud);
+                console.log('\n     ', 'Info de la ciudad'.cyan.underline);
+                console.log('ID: ', nombreLugarSeleccionado.id);
+                console.log('Nombre: ', nombreLugarSeleccionado.nombre);
+                console.log('Latitud: ', nombreLugarSeleccionado.latitud);
+                console.log('Longitud: ', nombreLugarSeleccionado.longitud);
+                console.log('\n     ', 'Clima de la ciudad'.cyan.underline);
+                console.log('Descripcion: ', clima.descripcion);
+                console.log('Temperatura Minima: ', clima.temperaturaMin);
+                console.log('Temperatura Maxima: ', clima.temperaturaMax);
+                console.log('Temperatura actual: ', clima.temperatura);
 
                 break;
             
             case 2: //Historial de busquedas
-
+                busquedas.historial.forEach((lugar, indice) => {
+                    indice = `${indice+1}`.cyan;
+                    console.log(`${indice}. ${lugar}`);
+                })
                 break;
         
             default:
--- models/busqueda.js
+++ models/busqueda.js
@@ -1,11 +1,13 @@
+const fs = require('fs')
 const axios = require('axios').default;
 
 class Busqueda {
 
-    historial = ['Bogota', 'Medellin', 'Cartagena'];
+    historial = [];
+    dbPath = './db/database.json'
 
     constructor() {
-
+        this.leerBD();
     }
 
     get parametrosMapBox() {
@@ -34,7 +36,7 @@ class Busqueda {
         try {
             const respuesta = await instance.get();
             // console.log(respuesta.data.features);
-            return respuesta.data.features.map( lugar => {
+            return respuesta.data.features.map(lugar => {
                 return {
                     id: lugar.id,
                     nombre: lugar.place_name,
@@ -47,7 +49,84 @@ class Busqueda {
             console.log(error);
         }
 
-        return []; //Aqui vamos a retornar todas las coincidencias de la peticion hecha por http
+    }
+
+
+    parametrosOpenWeather(latitud, longitud) {
+        return {
+            lat: latitud,
+            lon: longitud,
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'sp'
+        }
+    }
+
+    async consultarClimaCoords(latitud, longitud) {
+        //https://api.openweathermap.org/data/2.5/weather?lat=4.59889&lon=-74.08083&appid=b69543e3032213fcdf9c4257fadba1fc&units=metric&lang=sp
+
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: this.parametrosOpenWeather(latitud, longitud)
+            });
+
+            const respuesta = await instance.get();
+            // console.log(respuesta.data);
+
+            return {
+                descripcion: respuesta.data.weather[0].description,
+                temperaturaMin: respuesta.data.main.temp_min,
+                temperaturaMax: respuesta.data.main.temp_max,
+                temperatura: respuesta.data.main.temp
+            }
+
+
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    agregarHistorial(lugar = '') {
+
+        //Evitar historial repetido
+        if (this.historial.includes(lugar.toLowerCase())) {
+            return
+        }
+
+        this.historial = this.historial.splice(0,5); //Para que limite un historial maximo de 6 resultados
+
+        //Guardar historial
+        this.historial.unshift(lugar);
+
+        //Guardar en persistencia
+        this.guardarBD()
+
+    }
+
+    guardarBD() {
+
+        const payload = {
+            historial: this.historial
+        }
+
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload))
+    }
+
+    leerBD() {
+        //Verificar si existe la BD
+        if (!fs.existsSync(this.dbPath)) {
+            return null;
+        }
+
+        //En caso de existir que lea y asigne la info a una variable
+        const informacionString = fs.readFileSync(this.dbPath, { encoding: 'utf-8' }) //El encoding es para use un formato unicode
+        const informacion = JSON.parse(informacionString);
+        const {historial} = informacion;
+        this.historial = historial;
+        // console.log(historial);
     }
 }
 
